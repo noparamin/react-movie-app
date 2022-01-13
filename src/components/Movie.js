e3ff0818427b39deb42cf5fbb76a10a9373c46da
noparamin <noparamin@naver.com>
2022-01-13 17:31:03 +0900

    css 적용 grid

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,16 @@
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
+import styles from "./Movie.module.css";
 //링크를 클릭해서 다른 페이지로 넘어갈 때 Link를 사용하면 새로고침하지 않고 넘어가진다.
 
 function Movie({id, coverImg, title, summary, genres}) {
     return (
-    <div>
-        <img src={coverImg} alt={title} />
+    <div className={styles.movie}>
+        <img src={coverImg} alt={title} className={styles.movie__img} />
         <h2>
             <Link to={`/movie/${id}`}>{title}</Link> 
         </h2>
-        <p>{summary}</p>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
         <ul>
             {genres.map(g => <li key={g}>{g}</li>)}
         </ul>
