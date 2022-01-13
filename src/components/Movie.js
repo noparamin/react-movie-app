9ad1e8575ebab579969852c5a2b7af99dab77a64
noparamin <noparamin@naver.com>
2022-01-13 23:23:34 +0900

    css 적용 grid

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,17 +3,21 @@ import {Link} from "react-router-dom";
 import styles from "./Movie.module.css";
 //링크를 클릭해서 다른 페이지로 넘어갈 때 Link를 사용하면 새로고침하지 않고 넘어가진다.
 
-function Movie({id, coverImg, title, summary, genres}) {
+function Movie({id, coverImg, title, year, summary, genres}) {
     return (
     <div className={styles.movie}>
         <img src={coverImg} alt={title} className={styles.movie__img} />
-        <h2>
-            <Link to={`/movie/${id}`}>{title}</Link> 
-        </h2>
-        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul>
-            {genres.map(g => <li key={g}>{g}</li>)}
-        </ul>
+        <div>
+            <h2 className={styles.movie__title}>
+                <Link to={`/movie/${id}`}>{title}</Link> 
+            </h2>
+            <h3 className={styles.movie__year}>{year}</h3>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+            <ul className={styles.movie__genres}>
+                {genres.map(g => <li key={g}>{g}</li>)}
+            </ul>
+        </div>
+        
   </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -28,7 +28,7 @@ function Home() {
       <h1>Loading...</h1>
       ) : (
       <div className={styles.movies}>
-        {movies.map((movie) => <Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres}/>)}
+        {movies.map((movie) => <Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} year={movie.year} summary={movie.summary} genres={movie.genres}/>)}
       </div>
       )}
     </div>
