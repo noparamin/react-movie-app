ef2f58da81fe651d69c614a389d404b54210a5bb
noparamin <noparamin@naver.com>
2022-01-15 23:14:05 +0900

    nav bar 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,26 @@ function Nav() {
 
     return (
         <nav>
-            <div>
-            <ul className={style.ul}>
-                <div className={style.home}>
-                    <li className={style.li}><a href="../routes/Home.js">NETFELX</a></li>
+            <div className={style.header}>
+                <div className={style.section}>
+                    <div className={style.logo}>
+                        <a href="#">NETLEX</a>
+                    </div>
+                    <div className={style.nav}>
+                        <ul>
+                            <li><a href={`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`}>High Rating</a></li>
+                            <li><a href={`https://yts.mx/api/v2/list_movies.json?genres=action`}>Action</a></li>
+                            <li><a href={`https://yts.mx/api/v2/list_movies.json?genres=fantasy`}>Fantasy</a></li>
+                            <li><a href={`https://yts.mx/api/v2/list_movies.json?genres=sci-fi`}>Sci-Fi</a></li> 
+                        </ul>
+                    </div>
+                    <div className={style.sns}>
+                        <ul>
+                            <li>a</li>
+                            <li>b</li>
+                        </ul>
+                    </div>
                 </div>
-                
-                <li className={style.li}><a href={`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`}>High Rating</a></li>
-                <li className={style.li}><a href={`https://yts.mx/api/v2/list_movies.json?genres=action`}>Action</a></li>
-                <li className={style.li}><a href={`https://yts.mx/api/v2/list_movies.json?genres=fantasy`}>Fantasy</a></li>
-                <li className={style.li}><a href={`https://yts.mx/api/v2/list_movies.json?genres=sci-fi`}>Sci-Fi</a></li>                
-            </ul>
             </div>
         </nav>
         
